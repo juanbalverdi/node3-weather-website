621bb1e14bdd066d363ed63a047426127d3fb607
Juan <juan_ancap@protonmail.com>
2022-01-08 03:19:11 -0300

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener("submit", (e) => {
 
   const location = search.value;
 
-  fetch("http://localhost:3000/weather?search=" + location).then((response) => {
+  fetch("/weather?search=" + location).then((response) => {
     response.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error;
